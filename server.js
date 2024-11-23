fc6453dd9c2b5952c1977cdce43cb667b7af1944
Ramya mol R <ramya45r@gmail.com>
2024-11-23 16:25:09 +0530

    dockerfile added

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(bodyParser.json());
 const insightsRoutes = require("./routes/insightsRoutes");
 app.use("/api/insights", insightsRoutes);
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
