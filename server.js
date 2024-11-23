96d11b7da46c8eb7ba4919a125c161cd44fd6ae4
Ramya mol R <ramya45r@gmail.com>
2024-11-23 16:11:38 +0530

    dockerfile added

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
-
 //  Server
 const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
